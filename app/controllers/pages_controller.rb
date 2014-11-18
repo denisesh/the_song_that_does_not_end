47598adcf95f887ec1b72fb58df32ea481ac67ab
denisesh <denisesh@gmail.com>
2014-11-18 15:55:40 -0800

    completed lambs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,16 @@
 class PagesController < ApplicationController
+  def home
+  end
+
+  def verse_2
+  end
+
+  def verse_3
+  end
+
+  def verse_4
+  end
+
+  #add same for verse 3 and verse 4
 
 end
